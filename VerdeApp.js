6034dfb7a151de4645df3f76d00e2ebd44aebdb3
Nazareth Villa <vnazareth6@gmail.com>
2022-02-19 18:02:06 -0400

    added google-map section

--- VerdeApp.js
+++ VerdeApp.js
@@ -13,6 +13,13 @@
 
 
 
+//Notes FORM
+function notasFunct(){
+  document.querySelector("#popUpNotesS").style.display = "block";
+  document.querySelector("#popUPNotesBackground").style.display = "block";
+  document.querySelector("#recentCitiesAdded").style.display = "none";
+}
+
 //home button    
 function home(){
   document.querySelector("#popUpNotesS").style.display = "none";
@@ -21,18 +28,30 @@ function home(){
 }
 
 
-//Notes FORM
-function notasFunct(){
-  document.querySelector("#popUpNotesS").style.display = "block";
-  document.querySelector("#popUPNotesBackground").style.display = "block";
-  document.querySelector("#recentCitiesAdded").style.display = "none";
-}
+//INSERT GALLERY FORM HERE
 
-//city FORM 
+
+
+//CITY FORM 
 function myFunction(){
   document.querySelector("#popUp_superMain").style.display = "block";
   document.querySelector("#popUPBackground").style.display = "block";
   document.querySelector("#recentCitiesAdded").style.display = "none";
+}
+
+//function that will show error or done message after Save button
+function succedTryAgain(){
+    let x = document.forms["myForm"]["fname"].value;
+    var exito = document.querySelector("#successCard");
+    var errorCard = document.querySelector("#tryagaing");
+    //if pop up values are empty show error popup
+    if (x == ""){
+      errorCard.style.display = "block";
+    }
+    //debug success Message popup when values are entered
+    else{
+      exito.style.display = "block";
+    }
 }
 
 //Save Button NOT CONNECTED TO A DATABASE YET
@@ -41,11 +60,22 @@ function save(){
     document.querySelector("#popUPBackground").style.display = "none";
     document.querySelector("#Gallery").style.display = "block";
     document.querySelector("#recentCitiesAdded").style.display = "block";
-    document.querySelector("#successCard").display = "block";
-
-  //ADD if pop up values are empty show error popup
-
+    succedTryAgain();
 }
+
+
+//ERROR-SUCCESS BUTTONS OFF
+
+//Success message done button function using default parameters.
+const done = (success = document.querySelector("#successCard")) =>  success.style.display = "none";
+
+//tryAgain button function using default parameters.
+const tryAgaing = (tryagain = document.querySelector("#tryagaing") ) => tryagain.style.display = "none";
+
+
+
+
+
 
 
 //TABLES
@@ -54,7 +84,7 @@ function save(){
 //PRUEBA si puedes a#adir CLASSSes to this section
 //PRUEBA si puedes aaplicar: Creating String Using Literals (from freecodeCamp - ES6) 
 
-  var body = document.querySelector("#main");
+  var bodyTable = document.querySelector("#main");
   var tbl = document.querySelector("#tables");
     // create elements <tbody>
   var tblBody = document.createElement("tbody");
@@ -89,20 +119,21 @@ function save(){
   // append the <tbody> inside the <table>
   tbl.appendChild(tblBody);
   // put <table> in the <body>
-  body.appendChild(tbl);
+  bodyTable.appendChild(tbl);
 
 //Added a class to the tblBody
   tblBody.classList.add("tablebody");
 
 
+  //MAP API
 
+//Boston lat and long
 
-  //ERROR-SUCCESS BUTTONS
-
-
-//Success message done button function using default parameters.
-const done = (success = document.querySelector("#successCard")) =>  success.style.display = "none";
-
-/*ADD SECTION WHEN USER DOES NOT CLICK ON SAVE BUTTON IT SHOW ERRORCARD*/
-//tryAgain button function using default parameters.
-const tryAgaing = (tryagain = document.querySelector("#tryagaing") ) => tryAgaing.style.display = "none";
+function initMap(){
+  let options = {
+    zoom: 8,
+    center: { lat: 42.3601, lng: -71.0589}
+  }
+  //new Map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+}
