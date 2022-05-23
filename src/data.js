42f0e34f51eaa804914af73294f4ddaabf5a8eca
Nazareth Villa <vnazareth6@gmail.com>
2022-05-22 19:56:40 -0500

    fetch function

--- src/data.js
+++ src/data.js
@@ -1,9 +1,4 @@
-//asynchrounously fetching recently cities added
-  //retrieving json data
-
 export async function fetchCityData() {
   const response = await fetch("data/cityData.json");
   return await response.json();
 }
-
-
